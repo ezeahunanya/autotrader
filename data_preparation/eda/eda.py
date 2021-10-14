e1a29e7c441062edc7541e5f347162248abf318f
Eze Ahunanya <ezeahunanya@outlook.com>
2021-10-14 15:50:20 +0100

    Add price histogram

--- data_preparation/eda/eda.py
+++ data_preparation/eda/eda.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from sklearn.feature_selection import mutual_info_regression
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 def get_label_encoding_for_categoricals(df):
     '''
@@ -24,3 +28,26 @@ def make_mi_scores(X, y):
     mi_scores = mi_scores.sort_values(ascending=False)
     
     return mi_scores
+
+def plot_mi_scores(scores):
+
+
+    scores = scores.sort_values(ascending=True)
+    width = np.arange(len(scores))
+    ticks = list(scores.index)
+    plt.barh(width, scores)
+    plt.yticks(width, ticks)
+    plt.title("Mutual Information Scores")
+
+def plot_price_histogram(df):
+        
+    binsize = 1000
+    bins = np.arange(0, df.price.max()+binsize, binsize)
+
+    plt.figure(figsize=[8, 5])
+    plt.hist(data = df, x = 'price', bins = bins)
+    plt.xlabel('Price (£)')
+    plt.ylabel('Frequency')
+    plt.title('Price Histogram')
+
+    return plt.figure
