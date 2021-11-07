93311c2e6099f706b099bac41f6507469a74e22b
Eze Ahunanya <ezeahunanya@outlook.com>
2021-11-07 19:05:47 +0000

    Refactor mi scores plotting function

--- data_preparation/eda/eda.py
+++ data_preparation/eda/eda.py
@@ -21,7 +21,9 @@ def make_mi_scores(X, y):
     '''
     Return a data series with mutual infomation of the columns.
     '''
-    
+
+    X = get_label_encoding_for_categoricals(X)
+
     discrete_features = X.dtypes == int
     mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features)
     mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
