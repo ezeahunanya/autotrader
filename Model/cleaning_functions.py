6340decb68c6a673447c4b46158a2e4d7e389124
Eze Ahunanya <ezeahunanya@outlook.com>
2021-11-13 07:38:15 +0000

    Add drop columns function to module

--- Model/cleaning_functions.py
+++ Model/cleaning_functions.py
@@ -29,4 +29,21 @@ def load_data_from_database():
 
     return pd.DataFrame(full_results)
 
+def drop_columns(df):
+    '''
+    Drops columns from dataframe.
+    '''
 
+    columns_to_drop = ['max_loading_weight', 'zero_to_sixty_two', 'gross_vehicle_weight',   
+                       'price_deviation', 'price_deviation_type',  
+                       'price_rating', 'price_rating_label', 'zero_to_sixty',               
+                       'advert_id', 'seller_id', 'date_scraped', 'time_scraped',
+                       'vehicle_location_postcode', 'page_url',
+                       'ad_description', 'price_excluding_fees', 'no_admin_fees', 'manufactured_year_identifier',
+                       'vehicle_registration_mark', 'derivative_id', 'car_condition', 'minimum_kerb_weight', 'average_mileage',
+                       'mileage_deviation', 'mileage_deviation_type', 'number_of_photos', 
+                       'body_type', 'transmission', 'fuel_type', 'doors', 'seats', 'number_of_owners', 'emission_scheme',
+                       'vehicle_location_latitude', 'vehicle_location_longitude', 'imported',
+                       'number_of_photos', 'tax', 'cylinders', 'valves', 'combined', 'insurance_group']
+
+    return df.drop(columns_to_drop, axis=1)
