429321e4fc9913bb7b94e5a1e3cc3153996e781c
Brook <71849503+Primebrook@users.noreply.github.com>
2022-05-12 16:12:44 +0100

    added inverse transform for price - v hacky

--- autotrader/main.py
+++ autotrader/main.py
@@ -4,6 +4,7 @@ PROJ_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path = [PROJ_DIR] + sys.path
 from utils import  load_pickle, MODEL_FEATURES
 import pandas as pd
+import numpy as np
 
 from flask import Flask, request, render_template
 
@@ -14,7 +15,9 @@ def predict_controller(request: request) -> float:
     feature_values = pd.DataFrame(data=feature_values, index=[0])[MODEL_FEATURES].values 
 
     model = load_pickle(PROJ_DIR+'/models/price_prediction_model.pkl')
-    prediction = round(float(model.predict(feature_values)), 3)
+    inverse_transform = lambda x: np.exp(x)-1 # HAAACKY  this is cause we transform the target variable for the model, but this inverse transform should be done in model training. Fix later 
+    prediction = model.predict(feature_values)
+    prediction = round(float(inverse_transform(prediction)), 3)
 
     return prediction
 
