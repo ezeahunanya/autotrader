23d3919a08b2cb32543d6f0e38ea260715483ec4
Brook <71849503+Primebrook@users.noreply.github.com>
2022-05-12 15:06:54 +0100

    added load pick function and model features

--- utils.py
+++ utils.py
@@ -9,3 +9,26 @@ def save_as_pickle(obj: Any, filepath: str) -> None:
 def save_as_json(obj: Dict, filepath: str) -> None:
     with open(filepath, 'w') as fp:
         json.dump(obj, fp)
+
+def load_pickle(filepath: str) -> Any:
+    with open(filepath, 'rb') as handle:
+        obj = pickle.load(handle)
+    return obj
+
+MODEL_FEATURES = [  # picked these numerical features randomly just to get going. 
+    'manufactured_year', 
+    'mileage', 
+    'engine_size', 
+    'top_speed', 
+    'engine_power', 
+    'engine_torque', 
+    'height', 
+    'length', 
+    'wheelbase', 
+    'width', 
+    'fuel_tank_capacity', 
+    'boot_space_seats_up', 
+    'urban', 
+    'extra_urban', 
+    'co2_emissions'
+    ]
