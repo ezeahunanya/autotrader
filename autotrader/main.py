2d4c11d37ef510d858e87a4412c0c9b2304dc3e7
Brook <71849503+Primebrook@users.noreply.github.com>
2022-05-12 19:30:34 +0100

    added shap values to prediction payload

--- autotrader/main.py
+++ autotrader/main.py
@@ -5,28 +5,34 @@ sys.path = [PROJ_DIR] + sys.path
 from utils import  load_pickle, MODEL_FEATURES
 import pandas as pd
 import numpy as np
+from typing import Dict
 
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-def predict_controller(request: request) -> float:
+def predict_controller(request: request) -> Dict:
     feature_values = {feature: request.form.get(feature, type=float) for feature in MODEL_FEATURES}
-    feature_values = pd.DataFrame(data=feature_values, index=[0])[MODEL_FEATURES].values 
+    feature_values = pd.DataFrame(data=feature_values, index=[0])[MODEL_FEATURES].values.reshape(1, len(MODEL_FEATURES))
 
     model = load_pickle(PROJ_DIR+'/models/price_prediction_model.pkl')
+    shap_explainer = load_pickle(PROJ_DIR+'/models/price_prediction_shap_explainer.pkl')
     inverse_transform = lambda x: np.exp(x)-1 # HAAACKY  this is cause we transform the target variable for the model, but this inverse transform should be done in model training. Fix later 
     prediction = model.predict(feature_values)
-    prediction = round(float(inverse_transform(prediction)), 3)
+    prediction = round(float(inverse_transform(model.predict(feature_values))), 3)
+    prediction_payload = {
+        'price': prediction,
+        'shap_values': {feature:shap_value for feature, shap_value in zip(MODEL_FEATURES, shap_explainer(feature_values).values.tolist()[0])}
+    }
 
-    return prediction
+    return prediction_payload
 
 @app.route("/predict", methods=['GET', 'POST'])
 def predict():
     if request.method == 'GET':
         return render_template('predict.html')
     elif request.method == 'POST':
-        return {'price_prediction': predict_controller(request)}
+        return {'prediction_details': predict_controller(request)}
 
 
 
